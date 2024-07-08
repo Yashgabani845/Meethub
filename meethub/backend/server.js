c801f2f74b6074d71ac00e0e89edf210c9e75335
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-09 00:46:20 +0530

    backend starting done

--- meethub/backend/server.js
+++ meethub/backend/server.js
@@ -1,53 +1,29 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-
+const bodyParser=require('body-parser');
+const{Server}=require('socket.io');
+const { Socket } = require('socket.io-client');
+const io = new Server();
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "http://localhost:3000", // Frontend URL
-    methods: ["GET", "POST"]
-  }
+app.use(bodyParser.json());
+
+const joinednames = new Map();
+
+
+io.on('connection',(socket)=>{
+socket.on('join-room',(data)=>{
+    
+    const {name,code}=data;
+    console.log('user',name,'joined');
+    joinednames.set(code,socket.id);
+    socket.join(code);
+    socket.broadcast.to(code).emit('user-joined',{code});
+})
 });
 
-app.use(cors({
-  origin: "http://localhost:3000", // Frontend URL
-}));
 
-const users = {};
-
-io.on('connection', socket => {
-  socket.on('join-room', roomId => {
-    if (!users[roomId]) {
-      users[roomId] = [];
-    }
-    users[roomId].push(socket.id);
-
-    const otherUsers = users[roomId].filter(id => id !== socket.id);
-
-    socket.emit('all-users', otherUsers);
-
-    socket.on('send-offer', ({ offer, to }) => {
-      io.to(to).emit('receive-offer', { offer, from: socket.id });
-    });
-
-    socket.on('send-answer', ({ answer, to }) => {
-      io.to(to).emit('receive-answer', { answer, from: socket.id });
-    });
-
-    socket.on('send-candidate', ({ candidate, to }) => {
-      io.to(to).emit('receive-candidate', { candidate, from: socket.id });
-    });
-
-    socket.on('disconnect', () => {
-      users[roomId] = users[roomId].filter(id => id !== socket.id);
-      socket.broadcast.emit('user-disconnected', socket.id);
-    });
-  });
-});
-
-server.listen(5000, () => {
-  console.log('Server is listening on port 5000');
-});
+app.listen(5000,()=>{
+    console.log('server is running on 5000 port');
+})
+io.listen(5001,()=>{
+    console.log('io server is running on 5001');
+})
--- meethub/src/Components/Enterform.jsx
+++ meethub/src/Components/Enterform.jsx
@@ -4,8 +4,8 @@ const Enterform=()=>{
     return (
         <div className="form">
             <form action="/videocall">
-            <input type="text"  placeholder="Enter your name" name="name" />
-            <input type="text" placeholder="Enter 6-digit code" name="code" />
+            <input type="text"  className="name" placeholder="Enter your name" name="name" />
+            <input type="text" className="code" placeholder="Enter 6-digit code" name="code" />
             <button type="submit">Enter room</button>
             </form>
 
