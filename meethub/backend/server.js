0dd9d4937613a3aa15bc805746e2238de8daaf94
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-08 18:26:04 +0530

    changes added

--- meethub/backend/server.js
+++ meethub/backend/server.js
@@ -1,18 +1,43 @@
 const { Socket } = require('dgram');
 const express = require('express');
 const http=require('http');
+const { userInfo } = require('os');
+const { off } = require('process');
 const app=express();
 const server=http.createServer(app);
-const {Server} = require("socket.io")
-const io= new Server(server);
-// now our http request will be handled by app 
+const socketIo = require("socket.io")
+const io = socketIo(server);
+    // now our http request will be handled by app 
 // and out socket requests will be handlled by io
-
+const user= {};
 app.use(express.json());
 app.use(express.static("./public"))
-io.on('connection',(socket)=>{
-    console.log("new user is connected",socket,id);
-} )
+io.on('connection', socket =>{
+    socket.on('join-room',roomId =>{
+        if(!user[roomId]){
+            user[roomId]=[];
+        }
+        user[roomId].push(socket.id);
+        const otherUsers= user[roomId].filter(id=>id!==socket.id);
+        socket.emit('all-users',otherUsers);
+
+        socket.on('send-offer',({offer,to})=>{
+            io.to(to).emit('recive-answer',{offer,from:socket.id});
+        })
+        socket.on('send-answer', ({ answer, to }) => {
+            io.to(to).emit('receive-answer', { answer, from: socket.id });
+          });
+      
+          socket.on('send-candidate', ({ candidate, to }) => {
+            io.to(to).emit('receive-candidate', { candidate, from: socket.id });
+          });
+
+          socket.on('disconnet',()=>{
+            user[roomId]=user[roomId].filter(id=>is!==socket.id);
+            socket.broadcast.emit('user-disconnected',socket.id);
+          });
+    });
+});
 
 app.get('/',(req,res)=>{
     res.send('Server is running')
--- meethub/src/Components/Videocall.jsx
+++ meethub/src/Components/Videocall.jsx
@@ -1,23 +1,101 @@
-import React ,{useEffect,useRef,useState} from "react";
-const VideoCall = ()=>{
-    const [socket,setSocket] = useState(null);
-    const localVideoref=useRef(null);
-    const remoteVideoref=useRef(null);
-    const peerConnection = useRef(null);
-    const [isCallStarted,setIsCallStated]=useState(fasle);
+import React, { useEffect, useRef, useState } from 'react';
+import io from 'socket.io-client';
 
-    useEffect(()=>{
-        const s = io('http://localhost:3001')
-        setSocket(s);
-        s.on('message',handleSignalingData);
-        return () => {
-            s.disconnect();
-          };
-    },[])
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const VideoCall = () => {
+  const [socket, setSocket] = useState(null);
+  const [peers, setPeers] = useState([]);
+  const localVideoRef = useRef(null);
+  const peerConnections = useRef({});
+  const remoteVideoRefs = useRef({});
+
+  useEffect(() => {
+    const s = io('http://localhost:5000');
+    setSocket(s);
+
+    s.on('connect', () => {
+      const roomId = 'test-room'; // example room ID
+      s.emit('join-room', roomId);
+    });
+
+    s.on('all-users', users => {
+      users.forEach(userID => {
+        callUser(userID);
+      });
+    });
+
+    s.on('receive-offer', async ({ offer, from }) => {
+      const peerConnection = createPeerConnection(from);
+      await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      const answer = await peerConnection.createAnswer();
+      await peerConnection.setLocalDescription(answer);
+      socket.emit('send-answer', { answer, to: from });
+    });
+
+    s.on('receive-answer', async ({ answer, from }) => {
+      const peerConnection = peerConnections.current[from];
+      await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    });
+
+    s.on('receive-candidate', async ({ candidate, from }) => {
+      const peerConnection = peerConnections.current[from];
+      await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    });
+
+    s.on('user-disconnected', id => {
+      if (peerConnections.current[id]) {
+        peerConnections.current[id].close();
+        delete peerConnections.current[id];
+      }
+    });
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  const createPeerConnection = (userID) => {
+    const peerConnection = new RTCPeerConnection({
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' }
+      ]
+    });
+
+    peerConnections.current[userID] = peerConnection;
+
+    peerConnection.onicecandidate = (event) => {
+      if (event.candidate) {
+        socket.emit('send-candidate', { candidate: event.candidate, to: userID });
+      }
+    };
+
+    peerConnection.ontrack = (event) => {
+      const videoElement = document.createElement('video');
+      videoElement.srcObject = event.streams[0];
+      videoElement.autoplay = true;
+      remoteVideoRefs.current.appendChild(videoElement);
+    };
+
+    return peerConnection;
+  };
+
+  const callUser = async (userID) => {
+    const peerConnection = createPeerConnection(userID);
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    localVideoRef.current.srcObject = stream;
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.emit('send-offer', { offer, to: userID });
+  };
+
+  return (
+    <div>
+      <h1>Video Call</h1>
+      <video ref={localVideoRef} autoPlay  muted />
+      <div ref={remoteVideoRefs}></div>
+    </div>
+  );
+};
+
 export default VideoCall;
